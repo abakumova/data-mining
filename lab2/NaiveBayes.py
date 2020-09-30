8e4cb501abfbdeb63ebd60ae535fb7bd6b83d565
Viktoriia Abakumova <abakumovaviktory@gmail.com>
2020-09-30 23:09:41 +0300

    Lab 2 added calculation of probabilities

--- lab2/NaiveBayes.py
+++ lab2/NaiveBayes.py
@@ -2,8 +2,6 @@ from lab1.DataProcessing import *
 
 SET_WORD = "word"
 SET_TIMES = "times"
-SPAM_STATISTIC_FILE = 'spam_stat.csv'
-HAM_STATISTIC_FILE = 'ham_stat.csv'
 OUTPUT_DIRECTORY_WITH_DATA = '../lab1/output'
 OUTPUT_DIRECTORY = 'output'
 SPAM_OUTPUT_FILE = 'spam_words.csv'
@@ -25,13 +23,13 @@ def calculate_sum_of_word_frequency(file):
     return general_frequency
 
 
-def create_statistic_dictionary(file, general_frequency):
+def create_statistic_dictionary(file):
     statistic_dictionary = {}
     with open(OUTPUT_DIRECTORY_WITH_DATA + '/' + file) as csv_file:
         logging.info('Calculate statistic of word frequency')
         reader = csv_dict_reader(csv_file)
         for line in reader:
-            statistic_dictionary[line[SET_WORD]] = int(line[SET_TIMES]) / general_frequency
+            statistic_dictionary[line[SET_WORD]] = int(line[SET_TIMES])
     return statistic_dictionary
 
 
@@ -46,19 +44,54 @@ def create_statistic_file(file, statistic_dictionary):
             writer.writerow([key, value])
 
 
+def words_in_set(words):
+    logging.info('words_in_set')
+    amount = 0
+    for word in words:
+        amount += int(words[word])
+    return amount
+
+
+def words_not_in_set(words, message):
+    logging.info('words_not_in_set')
+    unknown_words = 0
+    known_words = words.keys()
+    for word in message:
+        if word not in known_words:
+            unknown_words += 1
+    return unknown_words
+
+
+def calculate_probability(words, message):
+    logging.info('calculate_probability')
+    words_amount_in_set = words_in_set(words)
+    unknown_words_amount = words_not_in_set(words, message)
+    probability = 1
+    for word in message:
+        word_count = 0
+        if word in words:
+            word_count = int(words[word])
+        probability *= (word_count + 1) / (words_amount_in_set + unknown_words_amount)
+    return probability
+
+
 def main():
-    # Prepare test set
-    general_spam_frequency = calculate_sum_of_word_frequency(SPAM_OUTPUT_FILE)
-    general_ham_frequency = calculate_sum_of_word_frequency(HAM_OUTPUT_FILE)
-
-    statistic_spam_dictionary = create_statistic_dictionary(SPAM_OUTPUT_FILE, general_spam_frequency)
-    statistic_ham_dictionary = create_statistic_dictionary(HAM_OUTPUT_FILE, general_ham_frequency)
-
-    create_statistic_file(SPAM_STATISTIC_FILE, statistic_spam_dictionary)
-    create_statistic_file(HAM_STATISTIC_FILE, statistic_ham_dictionary)
-
     message = input('Enter the message: ')
-    tokenized_msg = processor_line(message)
+    tokenized_message = processor_line(message)
+    print(tokenized_message)
+
+    statistic_spam_dictionary = create_statistic_dictionary(SPAM_OUTPUT_FILE)
+    statistic_ham_dictionary = create_statistic_dictionary(HAM_OUTPUT_FILE)
+
+    spam_probability = calculate_probability(statistic_spam_dictionary, tokenized_message)
+    ham_probability = calculate_probability(statistic_ham_dictionary, tokenized_message)
+
+    sum_of_probabilities = spam_probability + ham_probability
+    spam_normalized_probability = spam_probability / sum_of_probabilities
+    ham_normalized_probability = ham_probability / sum_of_probabilities
+
+    print("This message is SPAM with {} probability".format(spam_normalized_probability))
+    print("This message is HAM with {} probability".format(ham_normalized_probability))
 
 
 if __name__ == '__main__':
