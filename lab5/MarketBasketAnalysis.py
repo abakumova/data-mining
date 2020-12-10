5a72331333722bda7aa4cd3b802755761827619f
Viktoriia Abakumova <abakumovaviktory@gmail.com>
2020-12-10 22:15:08 +0200

    Lab 5 Market Basket Analysis

---
+++ lab5/MarketBasketAnalysis.py
@@ -0,0 +1,13 @@
+import time
+
+INITIAL_FILE_NAME = 'data/OnlineRetail.xlsx'
+
+start_time = time.time()
+
+
+def main():
+    print("*** %s seconds " % (time.time() - start_time))
+
+
+if __name__ == '__main__':
+    main()
